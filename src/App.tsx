64943cdf7f9e21782417cb4c99acaac6a75c31a4
Muhammad.Ghazanfar <Muhammad.Ghazanfar@ibex.co>
2025-03-23 01:14:13 +0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='mb-4'>Hello world!</h1>
+      <p>Muhammad Ghazanfar Khan</p>
+    </>
+  )
+}
+
+export default App
